acf1706271d023063640254eb57bee17d4c4bb3d
Eanan <Eanan@DESKTOP-73AHJJQ>
2022-06-23 10:18:20 +0100

    Add project files.

---
+++ eConcert/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using eConcert.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace eConcert.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
